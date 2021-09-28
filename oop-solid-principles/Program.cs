93ae5edce0b83e16067fe406b82a527614cdd107
Oscar Estrada <o.estrada1001@gmail.com>
2021-09-28 05:22:36 -0700

    Rearranged code to be able to showcase before and after.

--- oop-solid-principles/Program.cs
+++ oop-solid-principles/Program.cs
@@ -1,72 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
+using oop_solid_principles.principles;
 
 namespace oop_solid_principles
 {
     class Program
     {
-        List<Item> Items = new List<Item>();
 
-        float Total = 0f;
 
         static void Main(string[] args)
         {
-            Program cashRegister = new ();
+            BeforeSolid cashRegister = new BeforeSolid();
 
             cashRegister.createItem();
             cashRegister.CalculateTotal();
         }
 
-        public void createItem()
-        {
-            int ItemsNeeded = 5;
 
-            do
-            {
-                Console.WriteLine("------ Press ESC to exit. ------");
-                Console.WriteLine("Item Name:");
-
-                string ItemName = Console.ReadLine();
-
-                if (ItemName.Equals(""))
-                {
-                    break;
-                }
-
-                Console.WriteLine("Item Price:");
-                float ItemPrice = Single.Parse(Console.ReadLine());
-
-                this.AddItem(new Item(ItemName, ItemPrice));
-
-            } while (this.Items.Count < ItemsNeeded);
-        }
-
-        public void AddItem(Item item)
-        {
-            this.Items.Add(item);
-        }
-
-        public void CalculateTotal()
-        {
-            foreach (Item item in this.Items)
-            {
-                Console.WriteLine("Item: {0}, Price: {1}", item.Name, item.Price);
-                this.Total += item.Price;
-            }
-
-            Console.WriteLine("Total Amount: {0:C}", this.Total);
-        }
     }
 
-    public class Item
-    {
-        public string Name = "";
-        public float Price = 0;
-
-        public Item(string name, float price)
-        {
-            this.Name = name;
-            this.Price = price;
-        }
-    }
 }
---
+++ oop-solid-principles/principles/BeforeSolid.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oop_solid_principles.principles
+{
+    public class BeforeSolid
+    {
+        List<Item> Items = new List<Item>();
+
+        float Total = 0f;
+
+        public BeforeSolid()
+        {
+        }
+
+        public void createItem()
+        {
+            int ItemsNeeded = 5;
+
+            do
+            {
+                Console.WriteLine("Item Name:");
+
+                string ItemName = Console.ReadLine();
+
+                if (ItemName == "exit" || ItemName == "done")
+                {
+                    ItemsNeeded = 0;
+                } else
+                {
+                    Console.WriteLine("Item Price:");
+
+                    float ItemPrice = Single.Parse(Console.ReadLine());
+
+                    this.AddItem(new Item(ItemName, ItemPrice));
+                }
+
+            } while (this.Items.Count < ItemsNeeded);
+        }
+
+        public void AddItem(Item item)
+        {
+            this.Items.Add(item);
+        }
+
+        public void CalculateTotal()
+        {
+            foreach (Item item in this.Items)
+            {
+                Console.WriteLine("Item: {0}, Price: {1}", item.Name, item.Price);
+                this.Total += item.Price;
+            }
+
+            Console.WriteLine("Total Amount: {0:C}", this.Total);
+        }
+    }
+
+    public class Item
+    {
+        public string Name = "";
+        public float Price = 0;
+
+        public Item(string name, float price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
